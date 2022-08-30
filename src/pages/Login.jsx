75a7c1c28a153a5cd0357f7f96aa7dca73f09220
Felipe <luizbrutante@gmail.com>
2022-08-30 15:08:51 -0300

    Requisito 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
 
-class Login extends React.Component{
-    render(){
-        return(
-            <div data-testid="page-login">
-                Login
-            </div>
-        )
-    }
+class Login extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-login">
+        <form>
+          <input data-testid="login-name-input" />
+          <button type="button" data-testid="login-submit-button">
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
